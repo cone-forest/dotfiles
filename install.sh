07faa92579f0145f97a76abe6d78817ce4491414
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-02-02 01:45:31 +0300

    Beautiful install script

--- install.sh
+++ install.sh
@@ -1,80 +1,66 @@
 #!/bin/bash
 
-read -p "You sure you read the source code? (Y/n)" resp
+read -p "You sure you read the source code? (Y/n) " resp
+if [ -z "$resp" ] || [ "$resp" = "n" ]; then
+  echo "I am speechless..."
+  exit
+fi;
+
+spinner_t=jump
+
+# For beautiful spinning anomation
+yay -S gum --noconfirm >> install.log
 
 # Get system packages
-# Install neovim
-echo "Installing neovim..."
-echo "[=            ]   7%"
-yay -S neovim > install.log
-
 # Install programming tools
-echo "Installing git..."
-echo "[==           ]  15%"
-yay -S git >> install.log
+gum spin --spinner $spinner_t --title "Installing git..." -- yay -S git --noconfirm > install.log
+
+# Install neovim
+gum spin --spinner $spinner_t --title "Installing neovim..." -- yay -S neovim --noconfirm > install.log
 
 # lua
-echo "Installing lua..."
-echo "[===          ]  23%"
-yay -S lua lua-language-server >> install.log
+gum spin --spinner $spinner_t --title "Installing lua..." -- yay -S lua lua-language-server --noconfirm >> install.log
 
 # c/cpp
-echo "Installing c/cpp tools..."
-echo "[====         ]  30%"
-yay -S llvm clang clangd lldb codelldb cmake cmake-language-server clang-format cmake-lint >> install.log
+gum spin --spinner $spinner_t --title "Installing c/cpp tools..." -- yay -S llvm clang clangd lldb codelldb cmake cmake-language-server clang-format cmake-lint --noconfirm >> install.log
 
 # rust
-echo "Installing rustup..."
-echo "[=====        ]  38%"
-yay -S rustup >> install.log
+gum spin --spinner $spinner_t --title "Installing rustup..." -- yay -S rustup --noconfirm >> install.log
 
-echo "Installing rust stable toolchain..."
-echo "[======       ]  46%"
-rustup toolchain install stable >> install.log
+gum spin --spinner $spinner_t --title "Installing rust stable toolchain..." -- rustup toolchain install stable --noconfirm >> install.log
 
 # python
-echo "Installing python tools..."
-echo "[=======      ]  53%"
-yay -S python python-pynvim python-debugpy mypy perf ueberzug >> install.log
+gum spin --spinner $spinner_t --title "Installing python tools..." -- yay -S python python-pynvim python-debugpy mypy perf ueberzug --noconfirm >> install.log
 
 # golang
-echo "Installing golang tools..."
-echo "[========     ]  61%"
-yay -S go gopls >> install.log
+gum spin --spinner $spinner_t --title "Installing golang tools..." -- yay -S go gopls --noconfirm >> install.log
 
 # Install document tools
-echo "Installing document tools..."
-echo "[=========    ]  69%"
-yay -S texlive-langcyrillic texlive-xetex texlive-bin texlive-binextra texlive-latex texlive-latexextra texlive-luatex texlive-mathscience texlive-pictures texlive-revtex texlive-subfiles >> install.log
+gum spin --spinner $spinner_t --title "Installing document tools..." -- yay -S texlive-langcyrillic texlive-xetex texlive-bin texlive-binextra texlive-latex texlive-latexextra texlive-luatex texlive-mathscience texlive-pictures texlive-revtex texlive-subfiles --noconfirm >> install.log
 
-echo "Installing markdown..."
-echo "[==========   ]  76%"
-yay -S markdown >> install.log
+gum spin --spinner $spinner_t --title "Installing markdown..." -- yay -S markdown --noconfirm >> install.log
 
-echo "Installing ascii-image-converter..."
-echo "[===========  ]  84%"
-yay -S ascii-image-converter >> install.log
+gum spin --spinner $spinner_t --title "Installing ascii-image-converter..." -- yay -S ascii-image-converter --noconfirm >> install.log
 
 # Get packer
-echo "Installing packer and tmp..."
-echo "[============ ]  93%"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim >> install.log
-git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+gum spin --spinner $spinner_t --title "Installing packer..." -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim >> install.log
+gum spin --spinner $spinner_t --title "Installing tpm..." -- git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # You wish to install neovim config?
-read -p "You wish to install neovim config? (Y/n)" resp
+read -p "You wish to install neovim config? (Y/n) " resp
 if [ -z "$resp" ] || [ "$resp" = "y" ]; then
   cp -r nvim ~/.config/
   nvim -c PackerSync >> install.log
 fi;
 
 # You wish to install tmux config?
-read -p "You wish to install tmux config? (Y/n)" resp
+read -p "You wish to install tmux config? (Y/n) " resp
 if [ -z "$resp" ] || [ "$resp" = "y" ]; then
   cp -r tmux ~/.config/
   tmux source-file ~/.config/tmux/tmux.conf
 fi;
 
-echo "[=============] 100%"
-echo "Done!"
-
+gum style \
+	--foreground 212 --border-foreground 212 --border double \
+	--align center --width 50 --margin "1 2" --padding "2 4" \
+  'Done' 'Now you are cool ;)'
